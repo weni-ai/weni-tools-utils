ce8fdf284e439e64bf5443ea2a4c9739574ac7bd
Leonardo Amaral <lleonardoamaral2011@hotmail.com>
2026-03-09 11:32:49 -0300

    improve proxy request path construction.

--- tests/test.py
+++ tests/test.py
@@ -3,12 +3,12 @@ import os
 from dotenv import load_dotenv
 from weni.context import Context
 
-from src.weni_utils.tools.proxy import ProxyRequest
+from weni_utils.tools.proxy import ProxyRequest
 
 load_dotenv()
 
 context = Context(
-    parameters={"document": "11970011408"},
+    parameters={"document": "12345678900"},
     globals={},
     contact={},
     project={"auth_token": os.getenv("AUTH_TOKEN", "mock-token")},
@@ -18,8 +18,9 @@ context = Context(
 
 proxy = ProxyRequest(context)
 
+document = context.parameters.get("document")
 response = proxy.make_proxy_request(
-    path="api/oms/pvt/orders?q=11970011408",
+    path=f"api/oms/pvt/orders?q={document}",
     method="GET",
 )
 
